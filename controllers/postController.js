400c96db7e8d35b8a38c14ee3e85a33393e307fe
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-07 16:39:47 +0100

    feat[update-post]

--- controllers/postController.js
+++ controllers/postController.js
@@ -31,16 +31,40 @@ function store(req, res) {
     posts.push(newPost);
 
     // controlliamo
-    console.log(posts);
+    // console.log(posts);
 
-    // Restituiamo lo status corretto e la pizza appena creata
+    // Restituiamo lo status corretto e il post appena creato
     res.status(201);
     res.json(newPost);
 };
 
 // logica update
 function update(req, res) {
-    res.send('Modifica integrale del post' + req.params.id);
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id);
+    // cerchiamo il post tramite id
+    const post = posts.find(post => post.id === id);
+
+    // Piccolo controllo
+    if (!post) {
+        res.status(404);
+        return res.json({
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    // Aggiorniamo il post
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.image = req.body.image;
+    post.tags = req.body.tags;
+
+    // Controlliamo i posts
+    console.log(posts)
+
+    // Restituiamo il post appena aggiornato
+    res.json(post);
 };
 
 // logica modify
